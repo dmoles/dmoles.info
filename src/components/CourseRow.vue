fdb1d16805a9a289d874f85ab5f13713d60d057f
David Moles <dmoles@dmoles.net>
2022-09-08 15:38:07 -0700

    Customize site

---
+++ src/components/CourseRow.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const props = defineProps<{
+  id: number
+  name: string
+}>()
+
+const courseUrl = computed(() => `https://comminfo.rutgers.edu/academics/courses?courses=${props.id}&program=32`)
+
+</script>
+
+<template>
+  <tr>
+    <th scope="row">
+      <a :href="courseUrl">17:610:{{ id }}</a>
+    </th>
+    <td>
+      <a :href="courseUrl">{{ name }}</a>
+    </td>
+  </tr>
+</template>
---
+++ src/components/Profile.vue
@@ -0,0 +1,283 @@
+<script setup lang="ts">
+import selfie from '../assets/images/dm-selfie-2022-08-03-1024x1024.png'
+import CourseRow from './CourseRow.vue'
+
+const currentCourses = [
+  {
+    id: 501,
+    name: 'Introduction to Library and Information Professions'
+  },
+  {
+    id: 580,
+    name: 'Knowledge & Society'
+  },
+  {
+    id: 583,
+    name: 'Foundations of Preservation and Archives'
+  }
+]
+
+</script>
+
+<template>
+  <div class="profile">
+    <aside class="photo">
+      <figure>
+        <img :src="selfie" class="selfie" alt="Photo of David Moles">
+        <figcaption>Photo taken by the subject.</figcaption>
+      </figure>
+    </aside>
+    <section class="text">
+      <h1>David Moles</h1>
+
+      <table>
+        <tbody>
+          <tr>
+            <th scope="row">Student status</th>
+            <td>Online, part-time</td>
+          </tr>
+          <tr>
+            <th scope="row">Academic interests</th>
+            <td>
+              Archives and preservation, academic librarianship, information ethics,
+              indigenous and marginalized knowledge
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Current courses</th>
+            <td>
+              <table class="inner">
+                <tbody>
+                  <CourseRow v-for="{ id, name } in currentCourses" :id="id" :name="name"/>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Work status</th>
+            <td>
+              Full time
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Advisor</th>
+            <td>
+              <a href="https://comminfo.rutgers.edu/dalbello-marija">Marija Dalbello</a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Contact</th>
+            <td>
+              <table class="inner">
+                <tbody>
+                  <tr>
+                    <th scope="row">email:</th>
+                    <td><a href="mailto:david.moles@rutgers.edu">david.moles@rutgers.edu</a></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Twitter:</th>
+                    <td><a href="https://twitter.com/chronodm">@chronodm</a></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">My other website:</th>
+                    <td>
+                      <a href="https://dmoles.net">dmoles.net</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <section class="about">
+        <h2>About me</h2>
+        <p>
+          I’m attending from Albany, California, just north of
+          Berkeley, where I work as head of applications for Library
+          IT at the <a href="https://www.lib.berkeley.edu">UC Berkeley
+          Library</a>— roughly 50/50 as a software developer and as a
+          manager of other software developers.
+        </p>
+
+        <p>
+          I previously worked in digital preservation and data
+          publication at the California Digital Library (part of the
+          UC Office of the President in Oakland), and before that for
+          a library software vendor called Innovative Interfaces, so
+          I’ve been in and around libraries and archives for a
+          while. I like working here and I very much like working with
+          librarians and archivists, and I hope to keep doing both,
+          but ideally in a role that has me involved more directly and
+          creatively in GLAM work rather than just helping make other
+          people’s projects happen.
+        </p>
+
+        <p>
+          I have a BA in language studies (Japanese emphasis) from UC
+          Santa Cruz and an MSc in economic and social history from
+          Oxford University, and I’m looking forward to getting to
+          spend some time thinking about things like contested
+          historical narratives and the social construction of
+          knowledge after many years of decontextualized tech work.
+        </p>
+
+        <p>
+          I have two small children (ages 5 and 8) and I <a
+          href="https://dmoles.net/">sometimes write science
+          fiction</a>, although not so much since the small children
+          came along—and probably even less till I’ve completed the MI
+          program.
+        </p>
+
+        <footer>
+          &nbsp;
+        </footer>
+      </section>
+
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+@use '../colors';
+
+div.profile {
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 384px minmax(min-content, 1fr);
+
+  font-family: Arvo, 'serif';
+  color: black;
+
+  aside.photo {
+    grid-column: 1;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    color: colors.$cafe-cream;
+    background: colors.$oxford-blue;
+
+    figure {
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+
+      img.selfie {
+        width: 100%;
+        height: auto;
+      }
+
+      figcaption {
+        position: absolute;
+        left: -9999px;
+        top: -9999px;
+      }
+    }
+  }
+
+  section.text {
+    grid-column: 2;
+    padding: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+
+    background: colors.$cafe-cream;
+
+    h1 {
+      font-weight: 900;
+      font-size: 3rem;
+      line-height: 1.2em;
+      text-transform: uppercase;
+      font-family: Montserrat, 'sans-serif';
+    }
+
+    h2 {
+      font-weight: 900;
+      text-transform: uppercase;
+      font-size: 1rem;
+      line-height: 1.2em;
+      font-family: Montserrat, 'sans-serif';
+      margin-bottom: 0.9rem;
+    }
+
+    p {
+      font-size: 1rem;
+      line-height: 1.2em;
+      font-family: Arvo, 'serif';
+      margin-bottom: 0.9rem;
+    }
+
+    a {
+      color: colors.$aircraft-blue;
+      text-decoration-thickness: 0.5px;
+      text-decoration-skip-spaces: all;
+      text-underline-offset: 2px;
+
+      &:visited {
+        color: colors.$oxford-blue;
+      }
+    }
+
+    table {
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+
+      tr {
+        th {
+          vertical-align: top;
+          text-align: right;
+          font-weight: bold;
+          white-space: nowrap;
+          padding-right: 1rem;
+          text-transform: lowercase;
+        }
+
+        td, th {
+          font-size: 1rem;
+          padding-bottom: 0.6rem;
+          line-height: 1.2em;
+        }
+      }
+    }
+
+    table.inner {
+      margin: 0;
+
+      th {
+        font-weight: normal;
+        padding-right: 0.25rem;
+        text-align: left;
+      }
+
+      tbody {
+        tr {
+          &:last-of-type {
+            td, th {
+              padding-bottom: 0;
+            }
+          }
+        }
+      }
+    }
+
+    section.about {
+      margin-top: 1.2rem;
+
+      footer {
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+        border-bottom: 1px dotted colors.$jasper-blue;
+      }
+    }
+  }
+
+}
+</style>
