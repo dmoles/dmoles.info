1155bc43ad54d36c4768aebf5c4750e48b31e997
David Moles <dmoles@dmoles.net>
2022-09-08 12:18:21 -0700

    Set port to 3300

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+  server: {
+    port: 3300,
+    strictPort: true
+  },
   plugins: [vue()]
 })
