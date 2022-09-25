68b2b1e2d03617c229c1b194eaf22ab72ac32e83
David Moles <dmoles@dmoles.net>
2022-09-24 21:15:25 -0700

    Implement routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
-import Profile from './components/Profile.vue'
+import {computed, ref} from "vue";
+import type {Component, Ref} from "vue"
+import {onMounted, onUnmounted} from 'vue'
+import NotFound from './components/NotFound.vue'
+import Profile from './components/profile/Profile.vue'
+import Veracruz from './components/projects/583/veracruz/Veracruz.vue'
+
+
+// ------------------------------------------------------------
+// Window location/navigation state management
+
+const location: Ref<URL> = ref(new URL(window.location.toString()))
+
+const updateState: EventListener = (_event: Event) => {
+  const oldLocation = location.value.toString()
+  const newLocation = window.location.toString()
+  if (oldLocation !== newLocation) {
+    location.value = new URL(newLocation)
+  }
+}
+
+onMounted(() => window.addEventListener('popstate', updateState))
+onUnmounted(() => window.removeEventListener('popstate', updateState))
+
+// ------------------------------------------------------------
+// Routing
+
+const rootComponent: Component = Profile
+
+type Routes = { [k: string]: Component }
+const routes: Routes = {
+  '/': rootComponent,
+  '/projects/583/veracruz': Veracruz
+}
+
+const currentView = computed(() => {
+  const path = location.value.pathname
+  return routes[path] || NotFound
+})
+
 </script>
 
 <template>
-  <Profile/>
+  <component :is="currentView"/>
 </template>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="not-found">
+    <header>
+      <h1>Not found</h1>
+      <a class="button" href="/">Return to main page</a>
+    </header>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../colors';
+
+div.not-found {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+header {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+h1 {
+  font-weight: 900;
+  font-size: 3rem;
+  line-height: 1.2em;
+  font-family: Montserrat, 'sans-serif';
+  color: colors.$penguin;
+}
+
+a.button {
+  display: block;
+  font-family: Montserrat, 'sans-serif';
+
+  color: colors.$cafe-cream;
+
+  &:visited, &:active {
+    color: colors.$biarritz;
+  }
+}
+</style>
