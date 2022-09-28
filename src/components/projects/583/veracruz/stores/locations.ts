2ee1da0d9c9a9b0409639e3d16d039e178848b42
David Moles <dmoles@dmoles.net>
2022-09-27 21:05:34 -0700

    Get next/previous working

--- src/components/projects/583/veracruz/stores/locations.ts
+++ src/components/projects/583/veracruz/stores/locations.ts
@@ -5,13 +5,13 @@ import type { Location } from '../types/location'
 
 export const useLocationStore = defineStore('location', () => {
 
-    const veracruz: Ref<Location> = ref(<Location>[202, 953])
-    const mexicoCity: Ref<Location> = ref(<Location>[119, 947])
-    const portAuPrince: Ref<Location> = ref(<Location>[873, 975])
-    const washington: Ref<Location> = ref(<Location>[740, 316])
-    const colon: Ref<Location> = ref(<Location>[660, 1239])
-    const losAngeles: Ref<Location> = ref(<Location>[47, 487])
-    const tampico: Ref<Location> = ref(<Location>[153, 859])
+    const veracruz: Ref<Location> = ref(<Location>{ x: 202, y: 953, name: 'Veracruz' })
+    const mexicoCity: Ref<Location> = ref(<Location>{ x: 119, y: 947, name: 'Mexico City' })
+    const portAuPrince: Ref<Location> = ref(<Location>{ x: 873, y: 975, name: 'Port-au-Prince' })
+    const washington: Ref<Location> = ref(<Location>{ x: 740, y: 316, name: 'Washington, DC' })
+    const colon: Ref<Location> = ref(<Location>{ x: 660, y: 1239, name: 'Colón' })
+    const losAngeles: Ref<Location> = ref(<Location>{ x: 47, y: 487, name: 'Los Angeles' })
+    const tampico: Ref<Location> = ref(<Location>{ x: 153, y: 859, name: 'Tampico' })
 
     console.log('exporting washington = %o', washington)
 
--- src/components/projects/583/veracruz/types/ArchiveDoc.ts
+++ src/components/projects/583/veracruz/types/ArchiveDoc.ts
@@ -4,8 +4,6 @@ type ArchiveDoc = {
     id: string,
     src: string,
     location: Location,
-    iconImg?: HTMLElement, // TODO: remove this
-    docImg?: HTMLElement, // TODO: remove this
     date: Date,
     description: string,
     srcUrl: string,
