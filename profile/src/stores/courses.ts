cb253da6b2d0dc52adb9ca5adc84bbcba9b9b9fb
David Moles <dmoles@dmoles.net>
2023-08-05 16:09:18 +0200

    Sort past courses by ID

--- profile/src/stores/courses.ts
+++ profile/src/stores/courses.ts
@@ -68,6 +68,7 @@ export const useCoursesStore = defineStore('courses', () => {
     return terms.value
       .filter((t) => t < current)
       .flatMap((t) => coursesForTerm(t))
+      .sort((c1, c2) => c1.id - c2.id)
   })
 
   const allCourses: ComputedRef<Course[]> = computed(() => {
