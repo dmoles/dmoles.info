8d5c40fa91e5526e0cb859278f43a40be31d1210
David Moles <dmoles@dmoles.net>
2024-01-20 13:26:59 +0100

    Fix vite config to reflect removing Veracruz

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'path'
-import { defineConfig, resolveBaseUrl } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -13,9 +13,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        // TODO: make these dynamic
-        projects_583_veracruz: resolve(__dirname, 'projects/583/veracruz/index.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
